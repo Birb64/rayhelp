9821c58722a5c85eee7c4277f53c902aa407e65d
Birb64 <ryanaalley05@gmail.com>
2024-10-19 18:59:45 -0500

    More generalized

--- include/errorman.h
+++ include/errorman.h
@@ -3,67 +3,18 @@
 #include <vector>
 #include <string.h>
 #include <iostream>
-// Where the tech sounds are
-#define TECHSOUNDS_PATH     "audio/sounds/technical/"
-
+///
+//
+//      Created by Birb64 2024
+//      For use in RLE and 0x216
+//
+///
 using namespace std;
 
 #ifndef ERRORMAN_H
 #define ERRORMAN_H
 
-int maxTechSounds = 19;
-
-// To be removed
-const char* techsoundnames[19] = {
-"boot.wav", 
-"delete.wav", 
-"error1.wav", 
-"loaderror.wav", 
-"newfile.wav", 
-"oldfile.wav", 
-"openfile.wav", 
-"place.wav", 
-"question.wav", 
-"redo.wav", 
-"rotateFAST.wav", 
-"rotateSIDE.wav", 
-"rotateUP.wav", 
-"savesuccess.wav", 
-"snap.wav", 
-"technicalerror.wav", 
-"undo.wav", 
-"updatedenied.wav", 
-"updatesuccessful.wav"
-};
-
-// I wanted to make a "public" variable for LoadAllTechWaves, but I don't know how to yet, so here's this
-// ALSO: from now on after, I'll be using std::vector because it's an array but WAY more flexible
-std::vector<Wave> Standin;
-
-// Function to load all of the techy-type sound files like error1 and undo
-std::vector<Wave> LoadAllTechWaves(){
-
-    // Resets the Standin because it does not originate from this function
-    Standin = {};
-
-    // A for loop adding the path of TECHSOUNDS_PATH and the sound names together to add to Standin
-    for(int i = 0; i < maxTechSounds; i++){
-
-        // Combine the techsounds path and techsoundnames
-        std::string fullPath = std::string(TECHSOUNDS_PATH) + techsoundnames[i];
-        Wave trywave = LoadWave(fullPath.c_str());
-        if(trywave.sampleSize > 0){
-            Standin.push_back(trywave);
-        }
-        else{
-            printf("ERRORMAN_H COULD NOT LOAD %d", fullPath);
-        }
-    }
-
-    // Standin gets returned, the end
-    return Standin;
-}
-
+// A simpler way to debug booleans
 void DebugBoolean(bool b){
     if(b == true){
         printf("true\n");
@@ -79,20 +30,23 @@ void DebugBoolean(bool b, const char* t, const char* f){
     }
 }
 
+// A simpler way to debug Vectors 2 and 3
 void DebugVector3(Vector3 v3){
     std::string usethis;
     usethis = "X: " + std::to_string(v3.x) + " Y: " + std::to_string(v3.x) + " Z: " + std::to_string(v3.z) + "\n";
     printf(usethis.c_str());
 }
-
 void DebugVector2(Vector2 v2){
     std::string usethis;
     usethis = "X: " + std::to_string(v2.x) + " Y: " + std::to_string(v2.x) + "\n";
     printf(usethis.c_str());
 }
 
+// Don't ask why I have this here, in fact-
+/*
 void DebugString(std::string str){
     printf(str.c_str());
 }
-
+*/
+// There we go, fixed
 #endif
--- src/errorman.cpp
+++ src/errorman.cpp
@@ -1,12 +1,9 @@
 #include "../include/errorman.h"
 
-std::vector<Wave> LoadAllTechWaves();
-
-
+// A simpler way to debug booleans
 void DebugBoolean(bool b);
-
 void DebugBoolean(bool b, const char* t, const char* f);
 
+// A simpler way to debug Vectors 2 and 3
 void DebugVector3(Vector3 v3);
-
 void DebugVector2(Vector3 v2);
